33a94ef0ab20f98914e54419dcef780c44f41c1b
mikeshukshin <merqrylive@gmail.com>
2018-08-12 08:07:38 +0530

    fix etf contractInstance

--- client/models/etf.js
+++ client/models/etf.js
@@ -1,16 +1,6 @@
 import abi from './etfAbi';
 
-let contractInstance;
-
-const init = (localWeb3, address) => {
-  if (!contractInstance) {
-    contractInstance = new localWeb3.eth.Contract(abi, address);
-  }
-
-  return contractInstance;
-};
-
 export const factoryGetETFTokens = (localWeb3, address) => {
-  init(localWeb3, address);
+  let contractInstance = new localWeb3.eth.Contract(abi, address);
   return contractInstance.methods.getRequest().call();
 };
