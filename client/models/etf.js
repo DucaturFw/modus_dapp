1e70a15c7708e6b37ccffa637428961b5bdd235d
mikeshukshin <merqrylive@gmail.com>
2018-08-12 14:42:38 +0530

    Remove depricated contract interface and abi

--- client/models/etf.js
+++
@@ -1,6 +0,0 @@
-import abi from './etfAbi';
-
-export const factoryGetETFTokens = (localWeb3, address) => {
-  let contractInstance = new localWeb3.eth.Contract(abi, address);
-  return contractInstance.methods.getRequest().call();
-};
