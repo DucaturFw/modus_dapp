1b225d007d51f3ebab821689aa436bf8497da071
mikeshukshin <merqrylive@gmail.com>
2018-08-12 08:09:57 +0530

    Create regulator on creation token

--- client/actions/create.js
+++ client/actions/create.js
@@ -1,5 +1,7 @@
 import { createToken } from '../models';
 
+import tokenActions from './tokens';
+
 const createActions = {
   createToken: (tokensStake, amount) => (dispatch, getState) => {
     const erc20 = getState().tokens.erc20;
@@ -13,9 +15,17 @@ const createActions = {
       stakes.push(tokensStake[i].stake);
     }
 
+    setTimeout(() => {
+      tokenActions
+        .load()(dispatch, getState)
+        .then(() => {
+          window.history.pushState({ page: 'Detail' }, 'New ETF-Token', '/');
+        });
+    }, 60 * 1000);
+
     return createToken(address, stakes, amount)
       .then(result => {
-        // console.log('result', result);
+        console.log('result', result);
         const {
           events: {
             NewToken: { address }
@@ -28,6 +38,11 @@ const createActions = {
       })
       .then(({ address }) => {
         console.log('newToken', address);
+        tokenActions
+          .load()(dispatch, getState)
+          .then(() => {
+            window.history.pushState({ page: 'Detail' }, 'New ETF-Token', `/${address}`);
+          });
       });
   }
 };
